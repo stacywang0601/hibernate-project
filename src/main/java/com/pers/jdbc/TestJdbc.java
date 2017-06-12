d5fc434bbae5312dc3bf9907a4c82e4a315a5eda
stacywang0601 <stacywang61@gmail.com>
2017-06-12 13:06:07 -0400

    hiernate configuration

--- src/main/java/com/pers/jdbc/TestJdbc.java
+++ src/main/java/com/pers/jdbc/TestJdbc.java
@@ -10,12 +10,9 @@ public class TestJdbc {
 		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
 		String user = "hbstudent";
 		String pass = "hbstudent";
-
 		try {
 			System.out.println("Connecting to database: " + jdbcUrl);
-
 			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
-
 			System.out.println("Connection successful!!!");
 
 		} catch (Exception exc) {
