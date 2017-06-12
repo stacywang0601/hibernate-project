c1f1fd54aee03905737e9cb08a4720bd400fc07d
stacywang0601 <stacywang61@gmail.com>
2017-06-12 18:18:50 -0400

    query data using hibernate

---
+++ src/main/java/com/pers/hibernate/QueryStudent.java
@@ -0,0 +1,70 @@
+package com.pers.hibernate;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.pers.hibernate.entity.Student;
+
+public class QueryStudent {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+		            .addAnnotatedClass(Student.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			// query students
+			List<Student> theStudents = session.createQuery("from Student").getResultList();
+
+			// display the students
+			displayStudents(theStudents);
+
+			// query students: lastName='Zhang'
+			theStudents = session.createQuery("from Student s where s.lastName='Zhang'").getResultList();
+
+			// display the students
+			System.out.println("\n\nStudents who have last name of Zhang");
+			displayStudents(theStudents);
+
+			// query students: lastName='Zhang' OR firstName='Zheng'
+			theStudents = session.createQuery(
+			            "from Student s where" + " s.lastName='Zhang' OR s.firstName='Zheng'")
+			            .getResultList();
+
+			System.out.println("\n\nStudents who have last name of Zhang OR first name Zheng");
+			displayStudents(theStudents);
+
+			// query students where email LIKE '%gmail.com'
+			theStudents = session.createQuery("from Student s where" + " s.email LIKE '%gmail.com'")
+			            .getResultList();
+
+			System.out.println("\n\nStudents whose email ends with gmail.com");
+			displayStudents(theStudents);
+
+			// commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+		} finally {
+			factory.close();
+		}
+	}
+
+	private static void displayStudents(List<Student> theStudents) {
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
+	}
+
+}
